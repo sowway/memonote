f652b28273fb218b407f4aa4c86cc720484f22d2
sowway <sowway@hanmail.net>
2019-12-19 16:38:50 +0900

    octopusM
    octopusMove

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public GameObject Player;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Player.transform.position = new Vector2(1, 1);
+        }
+
+    }
+}
